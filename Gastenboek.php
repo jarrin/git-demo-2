d2900bef117a73fbe9ef045eed9f084bd1e95132
Jarrin van der Velden <j@rrin.nl>
2021-12-16 11:41:18 +0100

    Dit is mijn eerste commit!

---
+++ Gastenboek.php
@@ -0,0 +1,22 @@
+<?php
+$data = [
+    ["naam"=>"mark", "placed"=>"2021-10-5", "massage"=>"Dit is een bericht"],
+    ["naam"=>"willem", "placed"=>"2021-10-6", "massage"=>"Dit is een bericht"],
+    ["naam"=>"jan", "placed"=>"2021-10-7", "massage"=>"Dit is een bericht"]
+
+];
+var_dump($data);
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gastenboek</title>
+</head>
+<body>
+    
+</body>
+</html>
